100f980ba521f1743d1f21934cceb0a5e7f898fa
Kunxi Griswold <kgriswo1@binghamton.edu>
2019-07-23 01:25:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,7 +32,7 @@ def map_to_square(source_array)
   new_array = []
   i = 0
   while i < source_array.length
-    new_array << source_array[i]**
+    new_array << source_array[i]**2
     i+=1
   end
   new_array
