f15e18cbd8b0efba6032c8b0c8eb366c7d80a4b7
Kunxi Griswold <kgriswo1@binghamton.edu>
2019-07-23 01:25:38 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,3 +27,23 @@ def map_to_double(source_array)
   end
   new_array
 end
+
+def map_to_square(source_array)
+  new_array = []
+  i = 0
+  while i < source_array.length
+    new_array << source_array[i]**
+    i+=1
+  end
+  new_array
+end
+
+
+
+
+
+
+
+
+
+
