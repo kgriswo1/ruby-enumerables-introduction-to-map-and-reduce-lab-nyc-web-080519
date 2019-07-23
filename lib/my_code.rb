7135b1110bf504327aab7277ae7ffaf857adff92
Kunxi Griswold <kgriswo1@binghamton.edu>
2019-07-23 01:28:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,7 +38,14 @@ def map_to_square(source_array)
   new_array
 end
 
-
+def reduce_to_total(source_array, starting_point = 0)
+  sum = 0
+  while starting_point < source_array.length
+    sum += source_array[starting_point]
+    starting_point+=1
+  end
+  sum
+end
 
 
 
