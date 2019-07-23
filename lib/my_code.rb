6af3fcccf6ea437f2f895e060890758ea663ef77
Kunxi Griswold <kgriswo1@binghamton.edu>
2019-07-23 01:35:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,12 +48,11 @@ end
 #   sum
 # end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
   sum = 0
-  i = 0
-  while i < source_array.length
-    sum += source_array[i]
-    i+=1
+  while starting_point < source_array.length
+    sum += source_array[starting_point]
+    starting_point+=1
   end
   sum
 end
