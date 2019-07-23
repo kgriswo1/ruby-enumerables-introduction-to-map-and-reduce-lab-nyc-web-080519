48053ad4cb1e7ed4aea7c4334118178e3a12dd5e
Kunxi Griswold <kgriswo1@binghamton.edu>
2019-07-23 01:39:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,6 +47,14 @@ def reduce_to_total(source_array, starting_point = 0)
   starting_point
 end
 
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length
+    if source_array[i]
+      i+=1
+    end
+  end
+end
 
 
 
