60d0f5451c0c2a2153be037f4d4312bee34d5251
Kunxi Griswold <kgriswo1@binghamton.edu>
2019-07-23 01:42:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,7 +58,16 @@ def reduce_to_all_true(source_array)
   true
 end
 
-
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length
+    if source_array[i]
+      return true
+    end
+    i+=1
+  end
+  false
+end
 
 
 
